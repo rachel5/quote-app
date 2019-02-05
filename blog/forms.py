f3a9bd3b74ada7388f30634f6309ccf3048a90b8
Rachel Anderson <hello@sewlastweek.com>
2019-02-05 13:17:07 -0800

    Adds classes to form fields for use with Bootstrap styling.

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Post
 
 class PostForm(forms.ModelForm):
 
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.fields['title'].widget.attrs.update({'class': 'form-control'})
+    self.fields['text'].widget.attrs.update({'class': 'form-control'})
+
   class Meta:
     model = Post
-    fields = ('title', 'text',)
+    fields = ('title', 'text')
+    labels = {
+      'title': _('What do you want this to be called?'),
+      'text': _('What do you want to say?')
+    }
+
+
